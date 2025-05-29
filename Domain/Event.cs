7bca7ff7381e2c7b9ca9f67af011d0ecb36127af
Katherine Monroy <katherinemonroy@gmail.com>
2025-05-29 19:58:50 +0200

    Add Event entity

---
+++ Domain/Event.cs
@@ -0,0 +1,17 @@
+namespace Domain;
+
+public class Event
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+    public string Desription { get; set; }
+    public string Category { get; set; }
+    public bool IsCancelled { get; set; }
+    
+    // location props
+    public string City { get; set; }
+    public string Venue { get; set; }
+    public double Latitude { get; set; }
+    public double Longtitude { get; set; }
+}
--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+460ad53a8763cba52cfd1b525eb9fcfd17b5c304")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
