98afb3cd341c8270e2bfa065043e4d43d416eb63
Katherine Monroy <katherinemonroy@gmail.com>
2025-05-30 17:19:41 +0200

    Refactor: Inject Mediator in BaseApiController and simplify EventsController

--- API/Controllers/EventsController.cs
+++ API/Controllers/EventsController.cs
@@ -1,32 +1,23 @@
 using Application.Features.Events.Commands.DeleteEvent;
 using Application.Features.Events.Queries.GetEventDetails;
 using Application.Features.Events.Queries.GetEventsList;
-using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 public class EventsController : BaseApiController
 {
-    private readonly IMediator _mediator;
-
-    public EventsController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     [HttpGet]
     public async Task<ActionResult<List<EventListVm>>> GetEvents()
     {
-        var dtos = await _mediator.Send(new GetEventsListQuery());
+        var dtos = await Mediator.Send(new GetEventsListQuery());
         return Ok(dtos);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<EventListVm>> GetEventDetails(string id)
     {
-        var eventDto = await _mediator.Send(new GetEventDetailsQuery() { Id = id });
+        var eventDto = await Mediator.Send(new GetEventDetailsQuery() { Id = id });
 
         return Ok(eventDto);
     }
@@ -38,7 +29,7 @@ public class EventsController : BaseApiController
     public async Task<ActionResult> DeleteEvent(string id)
     {
         var deleteEventCommand = new DeleteEventCommand { Id = id };
-        await _mediator.Send(deleteEventCommand);
+        await Mediator.Send(deleteEventCommand);
         return NoContent();
     }
 }
