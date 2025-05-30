703bd69cedd8b89d38858551c698ab951e68d0a6
Katherine Monroy <katherinemonroy@gmail.com>
2025-05-30 14:59:09 +0200

    Add initialReact client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-red-500'>Mingo!</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
