61f6e272780a7dfe2df3afa033ce2f43173196f9
Katherine Monroy <katherinemonroy@gmail.com>
2025-05-30 17:42:04 +0200

    Add FluentValidation for CreateEvent command validation

---
+++ Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
@@ -0,0 +1,24 @@
+using Application.Contracts.Persistence;
+using Domain;
+using FluentValidation;
+
+namespace Application.Features.Events.Commands.CreateEvent;
+
+public class CreateEventCommandValidator : AbstractValidator<CreateEventCommand>
+{
+    public CreateEventCommandValidator()
+    {
+        RuleFor(e => e.Title)
+            .NotEmpty()
+            .WithMessage("{PropertyName} is required.")
+            .NotNull()
+            .MaximumLength(50)
+            .WithMessage("{PropertyName} must not exceed 50 characters.");
+
+        RuleFor(p => p.Date)
+            .NotEmpty()
+            .WithMessage("{PropertyName} is required.")
+            .NotNull()
+            .GreaterThan(DateTime.Now);
+    }
+}
