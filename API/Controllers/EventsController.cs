6638f1adbfff025ee0194b8e979baf90444cd47d
Katherine Monroy <katherinemonroy@gmail.com>
2025-05-30 13:52:32 +0200

    Add base API controller and events controller with GET endpoint

---
+++ API/Controllers/EventsController.cs
@@ -0,0 +1,23 @@
+using Application.Features.Events.Queries.GetEventsList;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class EventsController : BaseApiController
+{
+    private readonly IMediator _mediator;
+
+    public EventsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<EventListVm>>> GetEvents()
+    {
+        var dtos = await _mediator.Send(new GetEventsListQuery());
+        return Ok(dtos);
+    }
+}
