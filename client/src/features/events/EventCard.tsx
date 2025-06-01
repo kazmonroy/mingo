e162245338e6c3369fe210efad51db365b23f16c
Katherine Monroy <katherinemonroy@gmail.com>
2025-06-01 23:38:52 +0200

    Add zoom in effect on hover

--- client/src/features/events/EventCard.tsx
+++ client/src/features/events/EventCard.tsx
@@ -24,7 +24,11 @@ export const EventCard = ({ event }: { event: Event }) => {
       <SheetTrigger className='cursor-pointer'>
         <div className='flex items-center gap-4'>
           <div className='bg-zinc-100 size-18 rounded-md overflow-hidden'>
-            <img src='./images/1.jpg' alt='' />
+            <img
+              src='./images/1.jpg'
+              alt=''
+              className='w-full h-full object-cover transition-transform duration-300 hover:scale-110'
+            />
           </div>
 
           <div className='flex flex-col items-start'>
