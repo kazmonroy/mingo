5c3134b2a63eff969d587871c69cf70fd7b47a83
Katherine Monroy <katherinemonroy@gmail.com>
2025-06-02 11:42:20 +0200

    Add validation behavior to MediatR pipeline and register validators

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,3 +1,5 @@
+using Application.Exceptions;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -6,10 +8,15 @@ public static class ApplicationServiceRegistration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+      
         services.AddMediatR(cfg =>
-            cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
+            {
+                cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+                cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            }
         );
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
         return services;
     }
 }
