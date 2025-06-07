50b3c6d41fa858fb8f39b0a11bf9074b10b8665d
Katherine Monroy <katherinemonroy@gmail.com>
2025-06-07 12:45:01 +0200

    Enhance CreateEventCommandValidator with additional validation rules

--- Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
+++ Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
@@ -12,9 +12,21 @@ public class CreateEventCommandValidator : AbstractValidator<CreateEventCommand>
             .NotEmpty()
             .WithMessage("{PropertyName} is required.")
             .NotNull()
-            .MaximumLength(50)
-            .WithMessage("{PropertyName} must not exceed 50 characters.");
+            .MaximumLength(100)
+            .WithMessage("{PropertyName} must not exceed 100 characters.");
+        RuleFor(e => e.Description).NotEmpty().WithMessage("{PropertyName} is required.");
 
-        RuleFor(p => p.Date).NotEmpty().WithMessage("{PropertyName} is required.").NotNull();
+        RuleFor(e => e.Date)
+            .GreaterThan(DateTime.UtcNow)
+            .WithMessage("{PropertyName} must be in the future.");
+        RuleFor(e => e.Category).NotEmpty().WithMessage("{PropertyName} is required.");
+        RuleFor(e => e.City).NotEmpty().WithMessage("{PropertyName} is required.");
+        RuleFor(e => e.Venue).NotEmpty().WithMessage("{PropertyName} is required.");
+        RuleFor(e => e.Latitude)
+            .InclusiveBetween(-90, 90)
+            .WithMessage("{PropertyName} must be between -90 and 90 degrees.");
+        RuleFor(e => e.Longitude)
+            .InclusiveBetween(-180, 180)
+            .WithMessage("{PropertyName} must be between -180 and 180 degrees.");
     }
 }
