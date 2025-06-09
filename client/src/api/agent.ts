e8c41de91921490f6358839f41526e184c1c5e52
Katherine Monroy <katherinemonroy@gmail.com>
2025-06-09 16:07:05 +0200

    Enhance Axios response interceptor for improved error handling and user notifications

--- client/src/api/agent.ts
+++ client/src/api/agent.ts
@@ -1,4 +1,6 @@
+import { router } from '@/app/router/Routes';
 import axios from 'axios';
+import { toast } from 'sonner';
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => setTimeout(resolve, delay));
@@ -7,14 +9,39 @@ const agent = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-agent.interceptors.response.use(async (response) => {
-  try {
-    await sleep(500);
+agent.interceptors.response.use(
+  async (response) => {
+    await sleep(1000);
     return response;
-  } catch (error) {
-    console.error('Error in response interceptor:', error);
+  },
+  async (error) => {
+    await sleep(1000);
+    console.log('Axios Error:', error);
+
+    const { status } = error.response || {};
+
+    switch (status) {
+      case 400:
+        console.error('Bad Request:', error.response?.data);
+        toast(error.response?.data?.title || 'Bad Request');
+        break;
+      case 401:
+        console.error('Bad Request:', error.response?.data);
+        toast(error.response?.data?.title || 'Unauthorized');
+        break;
+      case 404:
+        toast(error.response?.data?.title || 'Not Found');
+        router.navigate('/not-found');
+        break;
+      case 500:
+        console.error('Server Error:', error.response?.data);
+        toast(error.response?.data?.title || 'Server Error');
+        break;
+      default:
+        break;
+    }
     return Promise.reject(error);
   }
-});
+);
 
 export default agent;
