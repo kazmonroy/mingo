a6684218990829e64b3ece5d3213c030e827d857
Katherine Monroy <katherinemonroy@gmail.com>
2025-07-07 14:03:03 +0200

    Add GetUserInfo endpoint to retrieve authenticated user details

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using Application.Features.Identity.Commands.RegisterUser;
+using Application.Features.Identity.Queries.GetCurrentUser;
 using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -24,7 +25,43 @@ public class AccountController : BaseApiController
         {
             return Ok();
         }
-          
+
         return BadRequest(result.Error);
     }
+
+    [AllowAnonymous]
+    [HttpGet("user-info")]
+    public async Task<ActionResult<UserInfoVm>> GetUserInfo()
+    {
+        if (User.Identity?.IsAuthenticated == false)
+        {
+            return NoContent();
+        }
+        var user = await _signInManager.UserManager.GetUserAsync(User);
+
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        return Ok(
+            new UserInfoVm
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Id = user.Id,
+                ImageUrl = user.ImageUrl
+            }
+        );
+
+       
+    }
+    
+    [HttpPost("logout")]
+    public async Task<ActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return NoContent();
+    }
+
 }
