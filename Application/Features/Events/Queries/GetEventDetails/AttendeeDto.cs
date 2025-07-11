869ccfb6c3de52de1cc861275ff5e7d8865ee721
Katherine Monroy <katherinemonroy@gmail.com>
2025-07-11 13:58:45 +0200

    Fix string.Empty for default string properties

--- Application/Features/Events/Queries/GetEventDetails/AttendeeDto.cs
+++ Application/Features/Events/Queries/GetEventDetails/AttendeeDto.cs
@@ -2,8 +2,8 @@ namespace Application.Features.Events.Queries.GetEventDetails;
 
 public class AttendeeDto
 {
-    public string Id { get; set; } = String.Empty;
-    public string DisplayName { get; set; } = String.Empty;
+    public string Id { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
     public string? Bio { get; set; }
     public string? ImageUrl { get; set; }
 }
--- Application/Features/Events/Queries/GetEventDetails/EventDetailsVm.cs
+++ Application/Features/Events/Queries/GetEventDetails/EventDetailsVm.cs
@@ -2,21 +2,20 @@ namespace Application.Features.Events.Queries.GetEventDetails;
 
 public class EventDetailsVm
 {
-    public string Id { get; set; }
-    public string Title { get; set; }
+    public string Id { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
     public DateTime Date { get; set; }
-    public string Description { get; set; }
-    public string Category { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
     public bool IsCancelled { get; set; }
-    public string HostDisplayName  { get; set; } = String.Empty;
-    public string HostId { get; set; } = String.Empty;
+    public string HostDisplayName { get; set; } = string.Empty;
+    public string HostId { get; set; } = string.Empty;
 
     // location props
-    public string City { get; set; }
-    public string Venue { get; set; }
+    public string City { get; set; } = string.Empty;
+    public string Venue { get; set; } = string.Empty;
     public double Latitude { get; set; }
     public double Longitude { get; set; }
-    
-    public ICollection<AttendeeDto> Attendees { get; set; } = new List<AttendeeDto>();
 
+    public ICollection<AttendeeDto> Attendees { get; set; } = new List<AttendeeDto>();
 }
