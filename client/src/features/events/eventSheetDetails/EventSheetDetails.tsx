ea832f2dd674aaf5dcc968c870b603c335938a02
Katherine Monroy <katherinemonroy@gmail.com>
2025-07-22 13:08:50 +0200

    Remove unused code

--- client/src/features/events/eventSheetDetails/EventSheetDetails.tsx
+++ client/src/features/events/eventSheetDetails/EventSheetDetails.tsx
@@ -160,34 +160,6 @@ export const EventSheetDetails = () => {
             </div>
           </section>
 
-          {/* <section>
-            <Card className='p-0 overflow-hidden gap-4'>
-              <CardHeader className='py-2 px-4 bg-muted items-center gap-0'>
-                <CardTitle className='text-sm'>Registration</CardTitle>
-              </CardHeader>
-              <CardContent className='px-4 space-y-4 pb-4'>
-                {event.isGoing ? (
-                  <>
-                    <p> Woho! You are going to this event!</p>
-
-                    <Button className='w-full' onClick={handleAttendance}>
-                      {isPendingAttendance
-                        ? 'Canceling...'
-                        : 'Cancel attendace'}
-                    </Button>
-                  </>
-                ) : (
-                  <>
-                    <p> Welcome! To join the event, please register below.</p>
-                    <Button className='w-full' onClick={handleAttendance}>
-                      {isPendingAttendance ? 'Joining...' : 'Join event'}
-                    </Button>
-                  </>
-                )}
-              </CardContent>
-            </Card>
-          </section> */}
-
           <ManageAttendance />
 
           <EventSheetHeader title='About Event'>
